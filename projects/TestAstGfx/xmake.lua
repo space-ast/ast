8826601ae0ac18fa586b10beac6627d399bd73a6
jinke18 <jinke18@foxmail.com>
2025-12-19 08:48:12 +0800

    feat: add OSG dependency check and disable targets if missing

--- projects/TestAstGfx/xmake.lua
+++ projects/TestAstGfx/xmake.lua
@@ -7,3 +7,6 @@ target("TestAstGfx")
     if is_plat("windows") then 
         add_defines("WIN32")
     end
+    if not has_package("openscenegraph") then 
+        set_enabled(false)
+    end 
--- src/AstGfx/xmake.lua
+++ src/AstGfx/xmake.lua
@@ -8,3 +8,6 @@
     if is_plat("windows") then 
         add_defines("WIN32")
     end
+    if not has_package("openscenegraph") then 
+        set_enabled(false)
+    end 
