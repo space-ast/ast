27224ae7c69079291567ea52ffde8c18aa931c59
axel <space_ast@163.com>
2026-01-29 21:44:57 +0800

    feat: add frame transform header and source files

---
+++ src/AstCore/Coordinate/FrameTransform.hpp
@@ -0,0 +1,38 @@
+///
+/// @file      FrameTransform.hpp
+/// @brief     ~
+/// @details   ~
+/// @author    axel
+/// @date      2026-01-29
+/// @copyright 版权所有 (C) 2026-present, ast项目.
+///
+/// ast项目（https://github.com/space-ast/ast）
+/// 本项目基于 Apache 2.0 开源许可证分发。
+/// 您可在遵守许可证条款的前提下使用、修改和分发本软件。
+/// 许可证全文请见：
+/// 
+///    http://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// 重要须知：
+/// 软件按"现有状态"提供，无任何明示或暗示的担保条件。
+/// 除非法律要求或书面同意，作者与贡献者不承担任何责任。
+/// 使用本软件所产生的风险，需由您自行承担。
+
+#pragma once
+
+#include "AstGlobal.h"
+#include "LocalOrbitFrame.hpp"
+#include "PlanetFrame.hpp"
+#include "EarthFrame.hpp"
+#include "MoonFrame.hpp"
+
+AST_NAMESPACE_BEGIN
+
+
+// @todo 实现通用的坐标转换函数
+// 参考orekit、GMAT的坐标系统类，还有spice的坐标转换函数
+// 实现从一个坐标系统到另一个坐标系统的转换
+
+
+
+AST_NAMESPACE_END
