016ad5c896bed4c70ba05cab7fa277b8bb8cf59a
axel <space_ast@163.com>
2026-05-13 20:16:25 +0800

    docs: update build config and improve API documentation

--- examples/xmake.lua
+++ examples/xmake.lua
@@ -3,9 +3,19 @@ set_default(false)
 add_deps("AstUtil", "AstCore", "AstMath", "AstSim", "AstTest", "AstSPICE")
 set_warnings("more")
 
-local files = os.files("**/*.c*")
+-- 添加 `examples` 和 `docs` 下面的所有cpp文件作为示例
+local patterns = {
+        "**/*.c*",
+        "../docs/guide/**.cpp",  
+        "../docs/examples/**.cpp",  
+        "../docs/dev/**.cpp",
+        "../docs/technical/**.cpp"
+}
+
+for _, pattern in ipairs(patterns) do
+    local files = os.files(pattern)
     for _, file in ipairs(files) do
-        local targetname = file:gsub("[\\/]", "_"):gsub("%.[^.]*$", "")
+        -- local targetname = file:gsub("[\\/]", "_"):gsub("%.[^.]*$", "")
         local basename = path.basename(file)
         target(basename)
             add_files(file)
@@ -14,4 +24,7 @@ local files = os.files("**/*.c*")
             if is_plat("windows") then
                 add_cxflags("/utf-8")
             end
+        target_end()
     end
+end
+
