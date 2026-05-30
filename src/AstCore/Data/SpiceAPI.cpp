a17eb7b400486011b6fd31c0163f4a609a6fc3d6
axel <space_ast@163.com>
2026-05-30 11:30:56 +0800

    refactor: implement phoenix  singleton pattern for ObjectManager and SpiceAPI, update SpiceBodyRegistry to use smart pointers

--- src/AstCore/Data/SpiceAPI.cpp
+++ src/AstCore/Data/SpiceAPI.cpp
@@ -139,7 +139,10 @@ SpiceAPI* SpiceAPI::Instance()
         singletonDestroyed = false;
         atexit([]() {
             if(!singletonDestroyed)
+            {
+                singletonDestroyed = true;
                 reinterpret_cast<SpiceAPI*>(&buf)->~SpiceAPI();
+            }
         });
     }
     return reinterpret_cast<SpiceAPI*>(&buf);
