6774835ebfd583f4f3ceece398de5d974a162880
space-axel <space_ast@163.com>
2026-08-30 04:14:24 +0000

    Deploying to gh-pages from @ space-ast/ast@2a4961254f230314cf7314d51b00db9d5185a56a 🚀

--- api/classast_1_1HPOPStateMapper.js
+++ api/classast_1_1HPOPStateMapper.js
@@ -1,7 +1,7 @@
 var classast_1_1HPOPStateMapper =
 [
     [ "fromState", "classast_1_1HPOPStateMapper.html#a9b6506bc3d8bde2fd2af0f9875d341e6", null ],
-    [ "setStateIndices", "classast_1_1HPOPStateMapper.html#a7b0b905d45acbe226a2a8a95219de411", null ],
+    [ "setFrameAndIndices", "classast_1_1HPOPStateMapper.html#a37fc3cfc9dcd082e46b5f49515d352d2", null ],
     [ "toState", "classast_1_1HPOPStateMapper.html#a08dd89aa45b285ec728c0876419cf383", null ],
     [ "toState", "classast_1_1HPOPStateMapper.html#a8e43dc5996e6497d93b68ec8c9800285", null ],
     [ "toStateSensitivityWrtDrag", "classast_1_1HPOPStateMapper.html#afcc7f8b017d898b461b987f566f6fe44", null ],
