543b67b498c4c8164932c9b6e4cd66ff43ec3257
axel <space_ast@163.com>
2026-09-01 09:05:48 +0800

    feat: add test support and improve Windows linking

--- scripts/xpack/port.lua
+++ scripts/xpack/port.lua
@@ -1,27 +1,33 @@
 -- examples 示例代码跨平台编译配置
 -- 首先安装c++编译器和xmake(https://xmake.io/zh/guide/quick-start.html)
--- 然后执行以下命令:
+-- 
+-- 然后执行以下命令: 
+-- 
 -- xmake
 -- xmake test
+-- xmake run <example>
+-- 
+-- 其中<example> 需要替换为具体的示例名称，例如: xmake run sgp4_teme_j2000
 
 add_rules("mode.release", "mode.debug")         -- release 模式和 debug 模式
 set_targetdir("bin")                            -- 输出目录
 add_includedirs("include")                      -- 头文件目录
 add_linkdirs("lib")                             -- 链接库目录
 
+-- 如果是 Windows 平台，添加 /utf-8 编码选项
+if is_plat("windows") then
+    add_cxflags("/utf-8")
+end
+
 -- 链接库
-if is_plat("windows") and is_mode("debug") then
-    add_links(
-        "AstAID", "AstAnalyzerD", "AstMockD",
-        "AstSPICED", "AstSimD", "AstOptD",
-        "AstCoreD", "AstMathD", "AstUtilD"
-    )
+if is_plat("windows") then
+    if is_mode("debug") then
+        add_links(os.files("lib/*D.lib"))
+    else
+        add_links(os.files("lib/*.lib|*D.lib"))
+    end
 else
-    add_links(
-        "AstAID", "AstAnalyzer", "AstMock",
-        "AstSPICE", "AstSim", "AstOpt",
-        "AstCore", "AstMath", "AstUtil"
-    )
+    add_links(os.files("lib/lib*.so"))
 end
 
 
@@ -37,10 +43,25 @@ for _, file in ipairs(files) do
         set_kind("binary")
         -- 添加到测试组
         add_tests("example")
-        -- 如果是 Windows 平台，添加 /utf-8 编码选项
-        if is_plat("windows") then
-            add_cxflags("/utf-8")
-        end
+        -- 设置分组名称
+        set_group("example")
+    target_end()
+end
+
+-- 遍历添加测试工程
+local files = os.files("test/**/*.c*")
+for _, file in ipairs(files) do
+    -- 提取文件名，作为目标名称
+    local basename = path.basename(file)
+    target(basename)
+        -- 添加源文件
+        add_files(file)
+        -- 设置为二进制目标
+        set_kind("binary")
+        -- 添加到测试组
+        add_tests("test")
+        -- 设置分组名称
+        set_group("test")
     target_end()
 end
 
