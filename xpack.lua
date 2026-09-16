cf88dec64d8b51f751e7f4d21ed9d1b8f8748bbf
space-axel <space_ast@163.com>
2026-09-16 20:46:11 +0800

    refactor: clean up Python file install and remove debug prints

--- xpack.lua
+++ xpack.lua
@@ -73,13 +73,13 @@ xpack("ast")
             if package:plat() == "windows" then
                 package:add("installfiles", "build/$(plat)/$(arch)/debug/*.pyd",   {prefixdir = "bin"})
                 package:add("installfiles", "build/$(plat)/$(arch)/release/*.pyd", {prefixdir = "bin"})
+                package:add("installfiles", "build/$(plat)/$(arch)/debug/*.py",   {prefixdir = "bin"})
             end
-            release_pyfiles = os.files("build/$(plat)/$(arch)/release/*.py")
-            debug_pyfiles = os.files("build/$(plat)/$(arch)/debug/*.py")
-            print("release_pyfiles: ", release_pyfiles)
-            print("debug_pyfiles: ", debug_pyfiles)
             package:add("installfiles", "build/$(plat)/$(arch)/release/*.py",   {prefixdir = "bin"})
-            package:add("installfiles", "build/$(plat)/$(arch)/debug/*.py",   {prefixdir = "bin"})
+            -- release_pyfiles = os.files("build/$(plat)/$(arch)/release/*.py")
+            -- debug_pyfiles = os.files("build/$(plat)/$(arch)/debug/*.py")
+            -- print("release_pyfiles: ", release_pyfiles)
+            -- print("debug_pyfiles: ", debug_pyfiles)
         end
     end)
 
