865c702c3ff2a48fef270d09e1305de4ebb49ad2
axel <space_ast@163.com>
2026-09-22 12:24:23 +0800

    feat: add asan mode to build rules and CI test matrix

--- xmake.lua
+++ xmake.lua
@@ -60,7 +60,7 @@ add_includedirs("include")
 -- 内置规则
 add_rules(
     "mode.debug", "mode.release", "mode.releasedbg", 
-    "mode.coverage"
+    "mode.coverage", "mode.asan"
 )                                                           -- 调试模式、发布模式、代码覆盖率模式
 -- add_rules("plugin.vsxmake.autoupdate")                      -- 自动更新vsxmake工程
 add_rules("c++.unity_build", {batchsize=20})                -- 开启unity build，提高编译效率
