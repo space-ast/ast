c36376cdf5624661c62b2c0821d5fb5fb2a30cf3
axel <space_ast@163.com>
2026-09-22 20:32:55 +0800

    build: update function of running script

--- test/xmake.lua
+++ test/xmake.lua
@@ -91,33 +91,42 @@ for _, file in ipairs(asc_files) do
         set_kind("phony")
         add_deps("ascript")
         add_tests("script")
-        local run = function(target)
+        local run = function(target, opt)
             import("core.project.project")
-            import("core.base.option")
+            import("private.action.run.runenvs")
+
             local target = project.target("ascript")
             if target then
-                -- print(target:name())
-                -- print(target:targetfile())
+                local outfile
+                local errfile
+                -- opt 为 nil 说明是 run，否则是 test，需要捕获输出和错误
+                if opt then
+                    outfile = os.tmpfile()
+                    errfile = os.tmpfile()
+                end
                 local targetfile = target:targetfile()
                 local runargs = {filepath}
-                local ok, syserrors
-                if option.get("verbose") then
-                    ok, syserrors = os.execv(targetfile, runargs, {try = true})
-                else
-                    local testdir = "./build/testoutput/"
-                    if not os.exists(testdir) then
-                        os.mkdir(testdir)
+                local addenvs, setenvs = runenvs.make(target)
+                local ok, syserrors = os.execv(targetfile, runargs, {
+                    try = opt ~= nil, curdir = target:rundir(),
+                    envs = runenvs.join(addenvs, setenvs),
+                    stdout = outfile, stderr = errfile
+                })
+                if opt then
+                    local outdata = os.isfile(outfile) and io.readfile(outfile) or ""
+                    local errdata = os.isfile(errfile) and io.readfile(errfile) or ""
+                    os.tryrm(outfile)
+                    os.tryrm(errfile)
+                    if #outdata > 0 then
+                        opt.stdout = outdata
                     end
-                    ok, syserrors = os.execv(targetfile, runargs, {try = true, stdout = testdir .. basename})
+                    if #errdata > 0 then
+                        opt.stderr = errdata
+                    end
                 end
-                -- print(ok, syserrors)
-                local passed = ok == 0
-                return passed
+                return ok == 0
             end
-            -- import("core.base.task")
-            -- task.run("run", {target="ascript", arguments={filepath}})
         end
         on_test(run)
         on_run(run)
-        -- add_extrafiles("xmake.lua")
 end
